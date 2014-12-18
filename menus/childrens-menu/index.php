b424858bad1586a94438d27d4e24c76a6c41190b
Ankit Patil <anankitpatil@gmail.com>
2014-12-18 09:13:37 +0000

    more before christmas

---
+++ menus/childrens-menu/index.php
@@ -0,0 +1,39 @@
+<?php include '../../header.php'; ?>
+<link rel="canonical" href="http://cobcheshire.com/menus/childrens-menu" />
+<title>Cock O' Barton | Children's Menu</title>
+<?php include '../../nav.php'; ?>
+<div class="container day-menu">
+  <div class="row">
+    <div class="col-lg-12 nopadding middlemenu">
+      <div class="col-sm-3 col-xs-6 mb24"><a href="../a-la-carte-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu A la carte">
+        <figure><img src="../../images/menus_menu_1.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu A la carte" /><span>A La carte menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="../day-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu day lunch">
+        <figure><img src="../../images/menus_menu_2.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu day lunch" /><span>Day menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="../sunday-lunch-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu sunday day lunch">
+        <figure><img src="../../images/menus_menu_3.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu sunday day lunch" /><span>Sunday lunch menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="./" title="Cock O' Barton, Bar & Restaurant Food and drinks menu children's">
+        <figure><img src="../../images/menus_menu_4.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu children's" /><span>Children's menu</span></figure>
+        </a></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-9 bodycopy">
+      <h1 class="mb48 mt48">Children's Menu</h1>
+      <div class="row">
+        <div class="col-lg-12 mt24 mb24 text-center">
+          <p class="lead"><i class="fa fa-circle fa-fw f12"></i> Children's Menu <i class="fa fa-circle fa-fw f12"></i></p>
+        </div>
+      </div>
+      <div class="editable" dir="menus/childrens-menu/" file="childrens-menu.php">
+        <?php include '../../editable/childrens-menu.php'; ?>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <?php include '../../banner.php'; ?>
+    </div>
+  </div>
+</div>
+<?php include '../../footer.php'; ?>
---
+++ menus/index.php
@@ -0,0 +1,42 @@
+<?php include '../header.php'; ?>
+<link rel="canonical" href="http://cobcheshire.com/menus" />
+<title>Cock O' Barton | Menus</title>
+<?php include '../nav.php'; ?>
+<div class="container menus">
+  <div class="row">
+    <div class="col-lg-12 nopadding middlemenu">
+      <div class="col-sm-3 col-xs-6 mb24"><a href="a-la-carte-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu A la carte">
+        <figure><img src="../images/menus_menu_1.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu A la carte" /><span>A La carte menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="day-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu Day lunch">
+        <figure><img src="../images/menus_menu_2.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu Day lunch" /><span>Day menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="sunday-lunch-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu Sunday day lunch">
+        <figure><img src="../images/menus_menu_3.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu Sunday day lunch" /><span>Sunday lunch menu</span></figure>
+        </a></div>
+      <div class="col-sm-3 col-xs-6 mb24"><a href="childrens-menu/" title="Cock O' Barton, Bar & Restaurant Food and drinks menu Children's">
+        <figure><img src="../images/menus_menu_4.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu Children's" /><span>Children's menu</span></figure>
+        </a></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-9 bodycopy justify">
+      <h1 class="mb48 mt48">Explore our menus</h1>
+      <p>Here at the CoB we spend an awful lot of time thinking and talking about food.</p>
+	  <p>Our chefs have come up with some great versions of some old favourites plus lots of new ideas and flavours you may never have tried before.  We are a big believer in Northwest is best and try to source lots of our ingredients from local regions, of course some of our ingredients must come from much further afield.</p>
+	  <p>We hope this all blends into a fine culinary experience for all of visitors, enjoy looking at the menus and gaining a taste for what we do here at the CoB.</p>
+      <div class="row middlemenu2 mt24">
+        <div class="col-lg-6 col-sm-6	"><a href="./cocktail-list" title="Cock O' Barton, Bar & Restaurant Food and drinks menu cocktail list">
+          <figure><img src="../images/menus_1.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu cocktail menu" /><span>Cocktail list</span></figure>
+        </div>
+        <div class="col-lg-6 col-sm-6"><a href="./wine-list" title="Cock O' Barton, Bar & Restaurant Food and drinks menu wine list">
+          <figure><img src="../images/menus_2.jpg" alt="Cock O' Barton, Bar & Restaurant Food and drinks menu wine list" /><span>Wine lists</span></figure>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <?php include '../banner.php'; ?>
+    </div>
+  </div>
+</div>
+<?php include '../footer.php'; ?>
